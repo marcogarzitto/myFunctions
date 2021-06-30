5dd4089d21a5e293c1e3da381526786ff41db968
Marco Garzitto <marcogarzitto@gmail.com>
2021-06-30 09:16:53 +0200

    Prova di aggiunta di librerie.

--- create_package.R
+++ create_package.R
@@ -21,7 +21,7 @@
 
 #
 
-# Depends: rJava, xlsx, car, psych, tidyverse, Hmisc
+# Depends: extrafont, officer, rJava, xlsx, car, psych, tidyverse, Hmisc
 # install.packages(c('rJava', 'xlsx', 'car', 'psych', 'tidyverse', 'Hmisc'), dep = TRUE)
 # update.packages(ask = 'graphics', checkBuilt = TRUE)
 
