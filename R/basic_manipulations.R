5042e732894c235f55115f8ead7068468d8a5014
Marco Garzitto <marcogarzitto@gmail.com>
2021-07-02 12:02:14 +0200

    Iniziate le funzioni di base per la manipolazione dei dati.

---
+++ R/basic_manipulations.R
@@ -0,0 +1,26 @@
+#' give_mean_confidence_intervals
+#'
+#' Funzione per ottenere l'intervallo di confidenza della media.
+#'
+#' @param x Vettore numerico.
+#' @param confidence Ampiezza dell'intervallo di confidenza voluto.
+#' @param distribution Usare una distribuzione z o t (per campioni piccoli o mal distribuiti).
+#' @return Un vettore (utile per inserirlo in un data-frame).
+#' @export
+give_mean_confidence_intervals <- function (x, confidence = 0.950, distribution = 'z')
+{
+ x <- psych::describe(x)
+ if (distribution == 'z')
+ {
+  plus_minus <- (abs(qnorm((1 - confidence) / 2)) * x$se)
+ } else if (distribution == 't')
+ {
+  plus_minus <- (qt(confidence + (1 - confidence) / 2, df = (x$n - 1)) * x$se)
+ } else
+ {
+  plus_minus <- 0
+ }
+ return(c(y = x$mean,
+          ymin = x$mean - plus_minus,
+          ymax = x$mean + plus_minus))
+}
