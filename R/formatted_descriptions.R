e485b0088f8137b5ef28611307f374c0abb6d917
Marco Garzitto <marcogarzitto@gmail.com>
2021-07-12 18:05:08 +0200

    Nuova funzione.

--- R/formatted_descriptions.R
+++ R/formatted_descriptions.R
@@ -4,7 +4,7 @@
 #'
 #' @param x Un vettore numerico (rappresentante una variabile continua).
 #' @param void_string Stringa da usare se il valore non c'è o non è calcolabile.
-#' @return Una stringa rappresentante il valore T, con vari abbellimenti.
+#' @return Un vettore con i risultati descrittivi.
 #' @export
 give_continuous_description <- function (x = NA, void_string = '-')
 {
@@ -32,6 +32,35 @@ give_continuous_description <- function (x = NA, void_string = '-')
              paste(out_md, ' ', '(', out_iqr, ')', sep = ''),
              paste('[', out_min, ' ', ',', ' ', out_max, ']', sep = ''))
            names(result) <- c('N (missing %)', 'Mean \u00B1SD', 'Md (IQR)', '[min , Max]')
+ return(result)
+}
+
+#' give_binary_description
+#'
+#' Funzione rappresentare le percentuali.
+#'
+#' @param x Un vettore di fattori (rappresentante una variabile binaria), convertito come tale se numerico.
+#' @param void_string Stringa da usare se il valore non c'è o non è calcolabile.
+#' @return Un vettore con i risultati descrittivi.
+#' @export
+give_binary_description <- function (x = NA, void_string = '-')
+{
+ X <- na.omit(x)
+ if ((length(X) < 3)) { return(void_string) }
+ if (length(levels(X)) != 2) { return(void_string) }
+ positive <- levels(X)[2]
+ out_n <- length(X)
+          out_n <- give_nice(value = out_n, decimals = 0, text = '', with_equal_sign = FALSE, with_sign = FALSE, min_value = -Inf, max_value = Inf, void_string = void_string)
+ out_miss <- 100 * (1 - (length(X) / length(x)))
+             out_miss <- give_nice_percent(value = out_miss, decimals = 1, text = '', with_equal_sign = FALSE, with_sign = FALSE, min_value = -Inf, max_value = Inf, void_string = void_string)
+ out_positive <- sum(X == positive)
+                 out_n <- give_nice(value = out_positive, decimals = 0, text = '', with_equal_sign = FALSE, with_sign = FALSE, min_value = -Inf, max_value = Inf, void_string = void_string)
+ out_positive_perc <- 100 * (sum(X == positive) / length(X))
+                      out_positive_perc <- give_nice_percent(value = out_positive_perc, decimals = 1, text = '', with_equal_sign = FALSE, with_sign = FALSE, min_value = -Inf, max_value = Inf, void_string = void_string)
+ #
+ result <- c(paste(out_n, ' ', '(', out_miss, ')', sep = ''),
+             paste(out_n, ' ', '(', out_miss, ')', sep = ''))
+           names(result) <- c('N (missing %)', paste(positive, ' ', '(', '%', ')', sep = ''))
  return(result)
 }
 
