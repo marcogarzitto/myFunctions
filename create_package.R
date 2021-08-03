b2bc1183ebbc145db75f1a67429dc8060bfce509
Marco Garzitto <marcogarzitto@gmail.com>
2021-08-03 11:48:13 +0200

    Update create_package.R

--- create_package.R
+++ create_package.R
@@ -22,7 +22,7 @@
 #
 
 # Depends: extrafont, magrittr, Hmisc, officer, rJava, xlsx, car, psych, tidyverse, sciplot, lme4, nlme, lmerTest
-# install.packages(c('extrafont', 'magrittr', 'Hmisc', 'officer', 'rJava', 'xlsx', 'car', 'psych', 'tidyverse', 'sciplot', 'lme4', 'nlme', 'lmerTest'), dep = TRUE)
+# install.packages(c('languageserver','extrafont', 'magrittr', 'Hmisc', 'officer', 'rJava', 'xlsx', 'car', 'psych', 'tidyverse', 'sciplot', 'lme4', 'nlme', 'lmerTest'), dep = TRUE)
 # update.packages(ask = 'graphics', checkBuilt = TRUE)
 
 #
